bd441cd7bc31c7c33805de694ccb4a7e6c61b171
tdwang <tdwang@Ctrip.com>
2017-11-21 20:41:23 +0800

    MLeaksFinder添加成员变量内存泄漏检测

---
+++ MLeaksFinder/NSSet+MemoryLeak.h
@@ -0,0 +1,17 @@
+//
+//  NSSet.h
+//  CTDebug
+//
+//  Created by 王腾达 on 2017/11/6.
+//  Copyright © 2017年 liuweiso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifdef MEMORY_LEAKS_ALL_OBJECT_FINDER_ENABLED
+@interface NSSet(MemoryLeak)
+
+- (BOOL)continueCheckObjecClass:(Class)objectClass;
+
+@end
+#endif
